479530d01b0934348111a7dfc3470ad3e4e8e8ba
Dmitrii Glazkov <dmitrii.m.glazkov@jettycloud.com>
2023-11-03 18:38:10 +0400

    initial commit

---
+++ frontend/.eslintrc.js
@@ -0,0 +1,39 @@
+module.exports = {
+  plugins: ['@typescript-eslint', 'prettier', 'unused-imports'],
+  extends: ['airbnb', 'airbnb-typescript', 'airbnb/hooks', 'next/core-web-vitals', 'prettier'],
+  parserOptions: {
+    project: './tsconfig.json',
+  },
+  rules: {
+    'no-magic-numbers': [
+      'error',
+      {
+        ignore: [-1, 0, 1],
+      },
+    ],
+    '@typescript-eslint/no-unused-vars': 'off',
+    'unused-imports/no-unused-imports': 'error',
+    'unused-imports/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+    '@typescript-eslint/naming-convention': [
+      'error',
+      {
+        selector: 'enumMember',
+        format: ['PascalCase'],
+      },
+      {
+        selector: 'typeLike',
+        format: ['PascalCase'],
+      },
+    ],
+    '@typescript-eslint/prefer-nullish-coalescing': 'error',
+    'react/no-array-index-key': 'off',
+    'react/no-unstable-nested-components': ['error', { allowAsProps: true }],
+    'react/jsx-filename-extension': [
+      'error',
+      {
+        extensions: ['.jsx', '.tsx', '.ts', '.js'],
+      },
+    ],
+    'react/button-has-type': 'warn',
+  },
+};
